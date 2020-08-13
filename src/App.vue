576da64d698f1b826e42728966f7d276851c6ab6
Kevin Song <kevinmnm2010@gmail.com>
2020-08-13 15:13:48 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <simple-shuffle></simple-shuffle>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import simpleShuffle from '@/components/simple_shuffle.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'simple-shuffle': simpleShuffle
   }
 }
 </script>
 
 <style>
+
+body{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    margin: 0 !important;
+    background: #101010;
+    padding: 0;
+    user-select: none;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/simple_shuffle.vue
@@ -0,0 +1,122 @@
+<template>
+    <div>
+        <transition-group name='shuffle-div' class='flex_box'>
+            <div class='box' v-for='all in arr' :key='all'>{{ all }}</div>
+        </transition-group>
+        <div class='button' @click='shuffler()'>Shuffle</div>
+        <div class='button' @click='auto($event)'>Auto Shuffle</div>
+        <div class='button' @click='stop($event)'>Stop</div>
+    </div>
+</template>
+
+
+<script>
+
+const _ = require('lodash');
+
+export default {
+    name: "simple-shuffle",
+    data(){
+        return {
+            arr: []
+        }
+    },
+    methods: {
+        shuffler(){
+        	this.arr =  _.shuffle(this.arr);
+        },
+        auto(e){
+        	this.shuffler();
+        	setInterval(()=>{
+            	this.shuffler();
+            },500);
+            e.target.style.display = 'none';
+        }
+    },
+    mounted(){
+        for (let i=0; i<100; i++){
+            this.arr.push(i);
+        }
+    }
+}
+
+</script>
+
+
+
+<style scoped>
+
+.shuffle-div-move{
+	transition: 0.5s;
+}
+
+.button{
+	margin:auto;
+    margin-top: 30px; 
+    display:block;
+    border: 1px solid lightGrey;
+    width: 10%;
+    height: 3%;
+    color: lightGrey;
+    text-align: center;
+}
+
+.button:hover{
+    color: lightGreen;
+    border-color: greenYellow;
+    animation-name: hov;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+}
+
+@keyframes hov{
+    0% {
+        border-radius: 0;
+        transform: scaleY(1);
+    }
+    25% {
+        border-radius: 5px;
+        transform: scaleY(1.1);
+    }
+    50% {
+        border-radius: 10px;
+        transform: scaleY(1.2);
+    }
+    75% {
+        border-radius: 15px;
+        transform: scaleY(1.3);
+    }
+    100% {
+        border-radius: 20px;
+        transform: scaleY(1.4);
+    }
+}
+
+.flex_box{
+	position: relative;
+    display: flex;
+    margin: auto;
+    border: 1px solid white;
+    flex-wrap: wrap;
+    flex-direction: row;
+    width: 380px;
+    margin-top: 20px;
+    box-shadow: 0 0 5px brown;
+    justify-content: space-between;
+    border-radius: 5px;
+}
+
+.box{
+	box-sizing: border-box;
+    width: 25px;
+    height: 25px;
+    border: 1px solid white;
+    margin: 5px;
+    color: greenYellow;
+    text-align: center; 
+    padding: 2px;
+}
+
+
+</style>
